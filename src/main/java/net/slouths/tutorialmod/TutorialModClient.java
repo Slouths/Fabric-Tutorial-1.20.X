dde6610f5edae81ddde4e24a054123015a2620cf
Slouths <nicholasnun06@gmail.com>
2024-07-17 16:27:22 -0400

    Added files

---
+++ src/main/java/net/slouths/tutorialmod/TutorialModClient.java
@@ -0,0 +1,9 @@
+package net.slouths.tutorialmod;
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
